f330878645b1854bf1b1a84858fb1f163a4becdd
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2024-08-07 17:15:49 +0200

    added tailwind

--- app.js
+++ app.js
@@ -11,18 +11,11 @@ app.use(express.static("./public"));
 // Use the session middleware
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
 
-// Access the session as req.session
-app.get('/', function(req, res, next) {
-  if (req.session.views) {
-    req.session.views++
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<p>views: ' + req.session.views + '</p>')
-    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
-    res.end()
-  } else {
-    req.session.views = 1
-    res.end('welcome to the session demo. refresh!')
-  }
+app.get("/signup", (req, res) => {
+    const options = {
+        root: __dirname
+    }
+    res.sendFile("public/auth/signup.html", options);
 })
 
 http.listen(process.env.PORT, () => {
