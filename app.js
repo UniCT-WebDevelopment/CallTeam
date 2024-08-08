f5da55c53a13c3d57ff7d69abc0f1034635d0bde
Salvo Polizzi <plzslv03a25c351k@studium.unict.it>
2024-08-08 16:39:20 +0200

    chore: updated folder structure

--- app.js
+++ app.js
@@ -4,19 +4,32 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const env = require("dotenv").config();
 const session = require("express-session");
+const path = require("path");
+
+//routers
+const publicRouter = require("./routes/public");
 
 app.use(express.json());
 app.use(express.static("./public"));
 
 // Use the session middleware
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
-
-app.get("/signup", (req, res) => {
-    const options = {
-        root: __dirname
+app.use(session(
+    { 
+        name: 'MyPersonalSession',
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.SECRET_KEY, 
+        cookie: {
+            secure: false, 
+            maxAge: 60000 
+        }
     }
-    res.sendFile("public/auth/signup.html", options);
-})
+))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(publicRouter);
 
 http.listen(process.env.PORT, () => {
     console.log(`Server in ascolto sulla porta ${process.env.PORT}`);
